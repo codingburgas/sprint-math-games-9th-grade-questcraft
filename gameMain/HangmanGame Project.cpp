d9bc61a2f5ce53ea362615337db480ff914d5a93
PDKapralev24 <PDKapralev24@codingburgas.bg>
2025-12-02 22:03:11 +0200

    add attempts, word guessing

--- gameMain/HangmanGame Project.cpp
+++ gameMain/HangmanGame Project.cpp
@@ -2,10 +2,49 @@
 #include <vector>
 using namespace std;
 
+void displayedWord(const vector<char>& guessed)
+{
+	for (char c : guessed)
+	{
+		cout << c << " ";
+	}
+	cout << endl;
+}
+
 int main()
 {
     cout << "Welcome to Hangman!" << endl;
 	string secretWord = "computer";
 	vector<char> guessed(secretWord.length(), '_');
+
+	int lives = 5;
+
+	cout << "The secret word has " << secretWord.length() << " letters." << endl;
+	displayedWord(guessed);
+
+	while (lives > 0) 
+	{
+		char guess;
+		count << "Enter your guess: ";
+		cin >> guess;
+
+		bool correct = false;
+		for (int i = 0; i < secretWord.length(); ++i) 
+		{
+			if (secretWord[i] == guess)
+		{
+				guessed[i] = guess;
+				correct = true;
+		}
+	}
+
+	if (!correct) 
+	{
+		lives--;
+		cout << "Wrong Guessed! Lives remain: " << lives << endl;
+	}
+	displayedWord(guessed);
+	}
 	return 0;
 }
+
