0b14dfa61132fb8346deb28941e336646df29555
PDKapralev24 <PDKapralev24@codingburgas.bg>
2025-12-03 22:40:59 +0200

    add started and credits menu

--- gameMain/HangmanGame Project.cpp
+++ gameMain/HangmanGame Project.cpp
@@ -23,51 +23,67 @@ bool wordIsGuessed(const vector<char>& guessed)
 
 int main()
 {
-    cout << "Welcome to Hangman!" << endl;
-	string secretWord = "computer";
-	vector<char> guessed(secretWord.length(), '_');
+	cout << "=== Welcome to Hangman! ===" << endl;
+	cout << "1. Start Game" << endl;
+	cout << "2. Credits" << endl;
+	cout << "Choose an option: " << endl;
 
-	int lives = 5;
+	int choice;
+	cin >> choice;
+	if (choice == 2) {
+		cout << "Credits:" << endl;
+		cout << "Created and Deisgned by QuestCraft Studios" << endl;
+	}
 
-	cout << "The secret word has " << secretWord.length() << " letters." << endl;
-	displayedWord(guessed);
 
-	while (lives > 0) 
-	{
-		char guess;
-		cout << "Enter your guess: ";
-		cin >> guess;
+	if (choice == 1) {
+  	  cout << "Welcome to Hangman!" << endl;
+		string secretWord = "computer";
+		vector<char> guessed(secretWord.length(), '_');
 
-		bool correct = false;
-		for (int i = 0; i < secretWord.length(); ++i) 
+		int lives = 5;
+
+		cout << "The secret word has " << secretWord.length() << " letters." << endl;
+		displayedWord(guessed);
+
+		while (lives > 0) 
 		{
-			if (secretWord[i] == guess)
-		{
-				guessed[i] = guess;
-				correct = true;
+			char guess;
+			cout << "Enter your guess: ";
+			cin >> guess;
+
+			bool correct = false;
+			for (int i = 0; i < secretWord.length(); ++i) 
+			{
+				if (secretWord[i] == guess)
+			{
+					guessed[i] = guess;
+					correct = true;
+			}
 		}
-	}
 
-	if (!correct) 
-	{
-		lives--;
-		cout << "Wrong Guessed! Lives remain: " << lives << endl;
-	}
-	displayedWord(guessed);
-	
-		if (wordIsGuessed(guessed))
+		if (!correct) 
 		{
-			cout << "Congratulations! You've guessed the word: " << secretWord << endl;
-			break;
+			lives--;
+			cout << "Wrong Guessed! Lives remain: " << lives << endl;
 		}
-	}
+		displayedWord(guessed);
 		
-		if (!wordIsGuessed(guessed))
-		{
-			cout << "Game Over! The secret word was: " << secretWord << endl;
+			if (wordIsGuessed(guessed))
+			{
+				cout << "Congratulations! You've guessed the word: " << secretWord << endl;
+				break;
+			}
 		}
+			
+			if (!wordIsGuessed(guessed))
+			{
+				cout << "Game Over! The secret word was: " << secretWord << endl;
+			}
+	}
 	
 	return 0;
 }
+
 
 
