400a99d75aebc8d8e182da2fa23faca3e6bcbc3c
PDKapralev24 <PDKapralev24@codingburgas.bg>
2025-12-07 23:20:19 +0200

    add return to main menu after game ends

--- gameMain/HangmanGame Project.cpp
+++ gameMain/HangmanGame Project.cpp
@@ -23,112 +23,115 @@ bool wordIsGuessed(const vector<char>& guessed)
 
 int main()
 {
-	cout << "=== Welcome to Hangman! ===" << endl;
-	cout << "1. Start Game" << endl;
-	cout << "2. Credits" << endl;
-	cout << "Choose an option: " << endl;
+	while (true) {
+		cout << "=== Welcome to Hangman! ===" << endl;
+		cout << "1. Start Game" << endl;
+		cout << "2. Credits" << endl;
+		cout << "Choose an option: " << endl;
 
-	int choice;
-	cin >> choice;
-	if (choice == 2) {
-		cout << "Credits:" << endl;
-		cout << "Created and Deisgned by QuestCraft Studios" << endl;
-	}
-
-
-	if (choice == 1) {
-		cout << "Welcome to Hangman!" << endl;
-
-		int difficulty;
-		int lives;
-		while (true) {
-			cout << "Choose difficulty level:" << endl;
-			cout << "1. Easy (infinite lives)" << endl;
-			cout << "2. Medium (5 lives)" << endl;
-			cout << "3. Hard (3 lives)" << endl;
-			cout << "Enter choice: ";
-			cin >> difficulty;
-
-			if (difficulty == 1) {
-				lives = -1;
-				break;
-			}
-			else if (difficulty == 2) {
-				lives = 5;
-				break;
-			}
-			else if (difficulty == 3) {
-				lives = 3;
-				break;
-			}
-			else {
-				cout << "Invalid level! Please choose again." << endl;
-			}
+		int choice;
+		cin >> choice;
+		if (choice == 2) {
+			cout << "Credits:" << endl;
+			cout << "Created and Deisgned by QuestCraft Studios" << endl;
 		}
-		vector <string> words = { "computer", "keyboard","mouse","monitor","printer" };
-		int wordChoice;
 
-		while (true) {
-			cout << "Choose a word by number (1 - " << words.size() << "): ";
-			cin >> wordChoice;
-				if (wordChoice >= 1 && wordChoice <= words.size()) {
+
+		if (choice == 1) {
+			cout << "Welcome to Hangman!" << endl;
+
+			int difficulty;
+			int lives;
+			while (true) {
+				cout << "Choose difficulty level:" << endl;
+				cout << "1. Easy (infinite lives)" << endl;
+				cout << "2. Medium (5 lives)" << endl;
+				cout << "3. Hard (3 lives)" << endl;
+				cout << "Enter choice: ";
+				cin >> difficulty;
+
+				if (difficulty == 1) {
+					lives = -1;
+					break;
+				}
+				else if (difficulty == 2) {
+					lives = 5;
+					break;
+				}
+				else if (difficulty == 3) {
+					lives = 3;
 					break;
 				}
 				else {
-					cout << "Invalid choice! Please enter a number between 1 and " << words.size() << "." << endl;
+					cout << "Invalid level! Please choose again." << endl;
 				}
 			}
-			string secretWord = words[wordChoice - 1];
-			vector<char> guessed(secretWord.length(), '_');
+			vector <string> words = { "computer", "keyboard","mouse","monitor","printer" };
+			int wordChoice;
 
-
-			cout << "The secret word has " << secretWord.length() << " letters." << endl;
-			displayedWord(guessed);
-
-			while (lives != 0)
-			{
-				char guess;
-				cout << "Enter your guess: ";
-				cin >> guess;
-
-				bool correct = false;
-				for (int i = 0; i < secretWord.length(); ++i)
-				{
-					if (secretWord[i] == guess)
-					{
-						guessed[i] = guess;
-						correct = true;
+			while (true) {
+				cout << "Choose a word by number (1 - " << words.size() << "): ";
+				cin >> wordChoice;
+					if (wordChoice >= 1 && wordChoice <= words.size()) {
+						break;
+					}
+					else {
+						cout << "Invalid choice! Please enter a number between 1 and " << words.size() << "." << endl;
 					}
 				}
+				string secretWord = words[wordChoice - 1];
+				vector<char> guessed(secretWord.length(), '_');
 
-				if (!correct)
-				{
-					if (lives == -1) {
-						cout << "Wrong Guess! Lives remain: infinite lives" << endl;
-					}
-					else
-					{
-						lives--;
-						cout << "Wrong Guess! Lives remain: " << lives << endl;
-					}
-				}
+
+				cout << "The secret word has " << secretWord.length() << " letters." << endl;
 				displayedWord(guessed);
 
-				if (wordIsGuessed(guessed))
+				while (lives != 0)
 				{
-					cout << "Congratulations! You've guessed the word: " << secretWord << endl;
-					break;
+					char guess;
+					cout << "Enter your guess: ";
+					cin >> guess;
+
+					bool correct = false;
+					for (int i = 0; i < secretWord.length(); ++i)
+					{
+						if (secretWord[i] == guess)
+						{
+							guessed[i] = guess;
+							correct = true;
+						}
+					}
+
+					if (!correct)
+					{
+						if (lives == -1) {
+							cout << "Wrong Guess! Lives remain: infinite lives" << endl;
+						}
+						else
+						{
+							lives--;
+							cout << "Wrong Guess! Lives remain: " << lives << endl;
+						}
+					}
+					displayedWord(guessed);
+
+					if (wordIsGuessed(guessed))
+					{
+						cout << "Congratulations! You've guessed the word: " << secretWord << endl;
+						break;
+					}
 				}
-			}
 
-			if (!wordIsGuessed(guessed))
-			{
-				cout << "Game Over! The secret word was: " << secretWord << endl;
+				if (!wordIsGuessed(guessed))
+				{
+					cout << "Game Over! The secret word was: " << secretWord << endl;
+				}
+				cout  << "Returning to main menu..." << endl << endl;
 			}
-		}
-
-		return 0;
 	}
+			return 0;
+}
+
 
 
 
